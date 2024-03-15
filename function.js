d45e611431f7333cd475d1ff0f9d47a69f12e2ef
devlrakib <devrakib5@gmail.com>
2024-03-15 23:22:09 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,12 @@
+ function startFan (){
+    console.log('This is the fan javascript code');
+    console.log('This is the fan javascript code of the year');
+    console.log('This is the fan javascript code in this month');
+ }
+ startFan ();
+ console.log('This is function one');
+//  startFan ();
+//  console.log('This is function two');
+//  startFan ();
+//  console.log('This is function three');
+ 
---
+++ parameter.js
@@ -0,0 +1,7 @@
+
+// ! Parameter Javascript Code
+function productPrice (money){
+    console.log('Mama Product Price ta bolen');
+    console.log(money);
+}
+productPrice (100);
